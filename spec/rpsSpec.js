bb6b962f0acfb9e62d3dd3f6e183af780707ec62
mntj <mntj11@gmail.com>
2014-06-08 19:00:46 -0400

    all tests passing. scores reset

--- spec/rpsSpec.js
+++ spec/rpsSpec.js
@@ -1,3 +1,29 @@
+describe("Move assignment and scoring", function() {
+
+  it("randomly assigns player 1 a move", function() {
+    var playerMove = moves.sample();
+    expect(typeof playerMove).toBe('string');
+  })
+
+  it("lets a player win if they get 3 sequential victories", function(){
+    var winner = isThereAWinner(3, 0);
+    expect(winner).toBe("Player 1 Wins!");
+  })
+
+  it("increments player1's score if they win a round", function() {
+    shoot('rock', 'scissors');
+    expect(onePlayerScore).toEqual(1);
+  })
+
+  it("resets players' scores", function() {
+    var onePScore = 3;
+    var twoPScore = 0;
+    isThereAWinner(onePScore, twoPScore);
+    expect(onePlayerScore).toEqual(0);
+  })
+
+})
+
 describe("Rock, Paper, Scissors Rules", function() {
 
   it('works', function(){
@@ -25,43 +51,7 @@ describe("Rock, Paper, Scissors Rules", function() {
   })
 
 });
-// 1st button will randomly assign a move to player 1
-describe("Move assignment and scoring", function() {
 
-  it("randomly assigns player 1 a move", function() {
-    var playerMove = moves.sample();
-    expect(typeof playerMove).toBe('string');
-  })
-
-  it("lets a player win if they get 3 sequential victories", function(){
-
-    var winner = isThereAWinner(3, 0);
-    expect(winner).toBe("Player 1 Wins!");
-  })
-
-
-
-
-})
-
-// Reset button - all scores go to zero
-// Reset/Start new game => var = 1
-// if var = 0, alert error
-
-
-// 1.press button 1
-// 2. randomly choose move for player 1 (sample from move array, assign move to player 1 move. store move in player1 move variable.
-
-// 3. press button 2
-// 4. randomly choose move for player 2 (sample from move array, assign move to player 2 move. store move in player2 move variable.
-
-// 5. pass player1 and player 2 moves into “shoot” function
-
-// 6.  increment winner’s score. First to 3 in a row wins. (if either player has 3 points more than the other, that player wins, game reset, display winner message, score reset)
-
-
-
-// 7. display score for each player.
 
 
 
--- src/rps.js
+++ src/rps.js
@@ -9,43 +9,69 @@ var randomMove = function () {
   return moves.sample();
 }
 
-var onePlayerMove = randomMove();
-var twoPlayerMove = randomMove();
+ onePlayerScore = 0;
+ twoPlayerScore = 0;
+
 
 function shoot (onePlayerMove, twoPlayerMove) {
   if (onePlayerMove === 'rock' && twoPlayerMove === 'scissors') {
+    onePlayerScore++;
+    console.log("Player one wins this round!");
     return true;
   }
 
   else if (onePlayerMove === 'scissors' && twoPlayerMove === 'paper') {
+    onePlayerScore++;
+    console.log("Player one wins this round!");
     return true;
   }
 
   else if (onePlayerMove === 'paper' && twoPlayerMove === 'rock') {
+    onePlayerScore++;
+    console.log("Player one wins this round!");
     return true;
   }
 
   else if (onePlayerMove === twoPlayerMove) {
+    console.log("Tie!")
     return undefined;
   }
 
   else {
+    console.log("Player two wins this round!");
+    twoPlayerScore++;
     return false;
   }
 }
 
-shoot(onePlayerMove, twoPlayerMove);
-
-var onePlayerScore = 0;
-var twoPlayerScore = 0;
-
-var isThereAWinner = function (onePlayerScore, twoPlayerScore) {
-      if ((Math.abs(onePlayerScore - twoPlayerScore)) === 3) {
-        if (onePlayerScore > twoPlayerScore) {
+function isThereAWinner (onePScore, twoPScore) {
+      if ((Math.abs(onePScore - twoPScore)) === 3) {
+        if (onePScore > twoPScore) {
+          console.log("Player 1 Wins!");
+          onePlayerScore = 0;
+          twoPlayerScore = 0;
           return "Player 1 Wins!";
         }
+
         else {
+          console.log("Player 2 Wins!");
+          onePlayerScore = 0;
+          twoPlayerScore = 0;
           return "Player 2 Wins!";
         }
       }
     }
+
+var game = function rockPaperScissors () {
+  var onePlayerMove = randomMove();
+  var twoPlayerMove = randomMove();
+  console.log("Player one chose "+onePlayerMove);
+  console.log("Player two chose "+twoPlayerMove);
+  console.log("rock paper scissors..shoot!");
+  shoot(onePlayerMove, twoPlayerMove);
+  isThereAWinner(onePlayerScore, twoPlayerScore);
+  console.log("Player 1's score: "+onePlayerScore);
+  console.log("Player 2's score: "+twoPlayerScore);
+};
+
+
